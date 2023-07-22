6846447beedc190f0cea7f33350872e3079cd9da
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-07-22 10:56:59 -0400

    type effect to main screen
    sorta lame type effect, kinda overdone these day, but what the heck did it anyway

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// Add custom JavaScript here
+// script for typying effect
+
+function typeWriter() {
+  const textArray = "I am Gary M. ";
+  const typingElement = document.getElementById("typing-text");
+  const typeDelay = 200;
+
+  typedText(textArray, typingElement, typeDelay);
+}
+
+function typedText(textArray, typingElement, typeDelay) {
+  for (let i = 0; i < textArray.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += textArray.charAt(i);
+    }, typeDelay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeWriter);
